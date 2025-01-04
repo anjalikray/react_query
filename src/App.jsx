688288233d28a0c381237f2e7dd0fb6ac1666ece
anjalikray <anjalikumari2907@gmail.com>
2025-01-04 16:08:28 +0530

    basic ui for react query practise

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./App.css";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import MainLayout from './components/Layouts/MainLayout'
 import Home from './pages/Home'
--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer>
+          <p>copyright @Anjali</p>
+        </footer>
+      );
 }
 
 export default Footer
